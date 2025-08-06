6e52b0be14ba7ca712c6f128d97099c9be49c9f5
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-08-06 22:22:42 +0530

    feat: integrate Convex for user data retrieval and update project configurations

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 import { useQuery } from "convex/react";
-import { api } from "@workspace/backend/convex/_generated/api";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/web</p>
-      {JSON.stringify(users)}
+      <div className="max-w-sm w-full mx-auto"> {JSON.stringify(users, null, 2)}</div>
     </div>
   );
 }
